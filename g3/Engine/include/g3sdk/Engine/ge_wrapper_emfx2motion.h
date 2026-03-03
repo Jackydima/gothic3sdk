2ed20f58b54b18e5006a1be6cf232fe9ef33ddec
Dimitri <dimitrybrusilouski@googlemail.com>
2026-03-04 00:14:02 +0100

    small refactroring

--- g3/Engine/include/g3sdk/Engine/ge_wrapper_emfx2motion.h
+++ g3/Engine/include/g3sdk/Engine/ge_wrapper_emfx2motion.h
@@ -22,15 +22,15 @@ class GE_DLLIMPORT eCWrapper_emfx2Motion : public bCObjectBase
 
     struct eSMotionDesc
     {
-        //GE_UNIMPL_STRUCT(eSMotionDesc)
+        // GE_UNIMPL_STRUCT(eSMotionDesc)
         // TODO SDK Update:
         GEFloat m_fFirst;
-        GEInt m_fSecond;
+        GEInt m_iSecond;
         GEFloat m_fAniSpeedScale;
-        GEInt m_fFourth;
+        GEInt m_iFourth;
         GEFloat m_fUnknown1;
         GEFloat m_fSixth;
-        GEInt m_fUnknown2;
+        GEInt m_iUnknown2;
     };
 
     // clang-format off
