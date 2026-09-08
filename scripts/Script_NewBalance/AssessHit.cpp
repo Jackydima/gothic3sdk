e454f906e185969f455f169a12c1ab07e0490c90
Dimitri Brusilovski <dimitrybrusilouski@gmail.com>
2026-09-08 21:56:22 +0200

    Script_Newbalance: Updated Zombie Stun resistant

--- scripts/Script_NewBalance/AssessHit.cpp
+++ scripts/Script_NewBalance/AssessHit.cpp
@@ -1932,6 +1932,26 @@ gEAction GE_STDCALL AssessHitNew(gCScriptProcessingUnit *a_pSPU, Entity *a_pSelf
         return gEAction_QuickStumble;
     }
 
+    // Special Zombie Protection
+    if (Victim.NPC.GetProperty<PSNpc::PropertySpecies>() == gESpecies_Zombie)
+    {
+        if (Victim.Routine.GetProperty<PSRoutine::PropertyAction>() == gEAction_PowerAttack
+            && Victim.GetCurrentAniPhase() != gEPhase_Recover
+            && Victim.GetCurrentAniPhase() != gEPhase_Raise
+            //&& DamagerOwnerAction != gEAction_PowerAttack
+            && DamagerOwnerAction != gEAction_HackAttack)
+        {
+            ScriptAdmin.CallScriptFromScript("PipiStumble", &Victim, &None, 0);
+            return gEAction_Stumble;
+        }
+
+        if ((IsNormalProjectileNB(Damager) || IsMagicProjectileNB(Damager)) && !isHeadshot)
+        {
+            ScriptAdmin.CallScriptFromScript("PipiStumble", &Victim, &None, 0);
+            return gEAction_Stumble;
+        }
+    }
+
     // Scream or make HitEffect, but no Stumble also processes logic when you hit someone, like setting up combat mode
     if ((GEInt)HitForce <= gEHitForce_Minimal
         && (GetHeldWeaponCategoryNB(DamagerOwner) == gEWeaponCategory_Ranged || IsInActiveAttack(Victim)))
