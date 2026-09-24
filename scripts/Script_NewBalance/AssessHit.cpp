316d32406a133f8884e7e302752c35f66b4f54fc
Dimitri Brusilovski <dimitrybrusilouski@gmail.com>
2026-09-24 17:10:14 +0200

    Script_Newbalance: Updated Stun protection for NPC switching weapons, by removing it during ZS_Attack_End and adding it to the old damage system

--- scripts/Script_NewBalance/AssessHit.cpp
+++ scripts/Script_NewBalance/AssessHit.cpp
@@ -1020,6 +1020,13 @@ gEAction GE_STDCALL AssessHitOld(gCScriptProcessingUnit *a_pSPU, Entity *a_pSelf
         return gEAction_QuickStumble;
     }
 
+    // New Protection System for AI trying to swap weapons
+    if (VictimAction == gEAction_Hold && !Victim.Routine.GetCurrentState().CompareFast("ZS_Attack_End"))
+    {
+        ScriptAdmin.CallScriptFromScript("PipiStumble", &Victim, &None, 0);
+        return gEAction_Stumble;
+    }
+
     // Scream or make HitEffect, but no Stumble also processes logic when you hit someone, like setting up combat mode
     if ((GEInt)HitForce <= gEHitForce_Minimal
         && (GetHeldWeaponCategoryNB(DamagerOwner) == gEWeaponCategory_Ranged || IsInActiveAttack(Victim)))
@@ -1977,7 +1984,7 @@ gEAction GE_STDCALL AssessHitNew(gCScriptProcessingUnit *a_pSPU, Entity *a_pSelf
     }
 
     // New Protection System for AI trying to swap weapons
-    if (VictimAction == gEAction_Hold)
+    if (VictimAction == gEAction_Hold && !Victim.Routine.GetCurrentState().CompareFast("ZS_Attack_End"))
     {
         ScriptAdmin.CallScriptFromScript("PipiStumble", &Victim, &None, 0);
         return gEAction_Stumble;
